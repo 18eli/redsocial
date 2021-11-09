e47cfc363cd0f523cd8602d3ae745f321189c353
18eli <0200311179@udh.edu.pe>
2021-11-09 16:46:25 -0500

    avance

--- app/src/main/java/com/example/redsocial/MainActivity.java
+++ app/src/main/java/com/example/redsocial/MainActivity.java
@@ -1,20 +1,19 @@
 package com.example.redsocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.firestore.DocumentReference;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.HashMap;
-import java.util.Map;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 public class MainActivity extends AppCompatActivity {
     FirebaseFirestore db = FirebaseFirestore.getInstance();
@@ -25,37 +24,48 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().hide();
     }
     public void siguiente(View view){
-        EditText dni= (EditText)findViewById(R.id.editTextTextPersonName);
-        String result = dni.getText().toString();
+        EditText dni= (EditText)findViewById(R.id.idDniMain);
+        String strDni = dni.getText().toString();
+        EditText clave = (EditText)findViewById(R.id.idClaveMain);
+        String strClave =clave.getText().toString();
+        db.collection("alumnos")
+                .whereEqualTo("dni",strDni)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if(task.isSuccessful()){
+                            if(task.getResult().isEmpty()){
+                                Toast.makeText(getApplicationContext(),"La cuenta no existe",Toast.LENGTH_SHORT).show();
+                            } else{
+                                for(QueryDocumentSnapshot documents : task.getResult()){
+                                    if (strClave.equals(documents.getData().get("clave").toString()))
+                                    {
+                                        Bundle extras = new Bundle();
+                                        extras.putString("nombre",documents.getData().get("nombreApellido").toString());
+                                        Intent siguiente = new Intent (MainActivity.this,principal.class);
+                                        siguiente.putExtras(extras);
+                                        startActivity(siguiente);
+                                    }else{
+                                        Toast.makeText(getApplicationContext(), "Clave incorrecta", Toast.LENGTH_SHORT).show();
+                                    }
 
-        Bundle extras = new Bundle();
+                                }
+                            }
 
-        extras.putString("nombre",result);
-        Intent siguiente = new Intent (this,principal.class);
-        siguiente.putExtras(extras);
-        startActivity(siguiente);
+                        }else{
+                            Toast.makeText(getApplicationContext(), "paso por aqui", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                });
+
+
     }
 
     public void registro(View view){
-        // Create a new user with a first and last name
-        Map<String, Object> user = new HashMap<>();
-        user.put("first", "Ada");
-        user.put("last", "Lovelace");
-        user.put("born", 1815);
-        db.collection("users")
-                .add(user)
-                .addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
-                    @Override
-                    public void onSuccess(DocumentReference documentReference) {
-
-                    }
-                })
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(Exception e) {
-
-                    }
-                });
+      Intent registro = new Intent (this,Registro.class);
+        startActivity(registro);
     }
 
 }
--- app/src/main/java/com/example/redsocial/Registro.java
+++ app/src/main/java/com/example/redsocial/Registro.java
@@ -1,14 +1,112 @@
 package com.example.redsocial;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class Registro extends AppCompatActivity {
+
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
+        Spinner spinner = (Spinner) findViewById(R.id.facultad);
+        String[] facultades = new String[] {"Ingenieria", "Educacion", "Ciencias Contables", "Medicina", "Derecho"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_spinner_item, facultades);
+        spinner.setAdapter(adapter);
+    }
+    public void registrar (View view){
+        EditText eTDni= (EditText)findViewById(R.id.idDni);
+        String dni = eTDni.getText().toString();
+        EditText eTcodigoU = (EditText)findViewById(R.id.idCodigoU);
+        String codigoU = eTcodigoU.getText().toString();
+        EditText eTclave = (EditText)findViewById(R.id.idClave);
+        String clave = eTclave.getText().toString();
+        EditText eTnomape = (EditText)findViewById(R.id.idNomApe);
+        String nomape = eTnomape.getText().toString();
+        Spinner spinner = (Spinner) findViewById(R.id.facultad);
+        String text = spinner.getSelectedItem().toString();
+
+        Map<String, Object> user = new HashMap<>();
+        user.put("dni", dni);
+        user.put("codigoUniversitario", codigoU);
+        user.put("clave", clave);
+        user.put("nombreApellido", nomape);
+        user.put("facultad",text);
+
+        db.collection("alumnos")
+                .whereEqualTo("dni",dni)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if(task.isSuccessful()){
+                    if(task.getResult().isEmpty()){
+                        db.collection("alumnos").document(dni).set(
+                                  user
+                        );
+                        Bundle extras = new Bundle();
+                        extras.putString("nombre",nomape);
+                        Intent salir = new Intent (Registro.this,principal.class);
+                        salir.putExtras(extras);
+                        startActivity(salir);
+                    } else{
+                        Toast.makeText(getApplicationContext(), "si existe el dni", Toast.LENGTH_SHORT).show();
+                    }
+                      for(QueryDocumentSnapshot documents : task.getResult()){
+
+                      }
+                }else{
+                    Toast.makeText(getApplicationContext(), "paso por aqui", Toast.LENGTH_SHORT).show();
+
+//                    db.collection("alumnos").document(dni).set(
+//                                     user
+//                             );
+
+                }
+            }
+
+        });
+
+    }
+
+    public void validacion (){
+
+
+//        db.collection("users")
+//                .get()
+//                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(Task<QuerySnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            for (QueryDocumentSnapshot document : task.getResult()) {
+//
+//
+//                            }
+//                        } else {
+//
+//                        }
+//                    }
+//                });
+
     }
 }
